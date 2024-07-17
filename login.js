8924ecba91e016324c7ccf2afb16b6f118322e16
Alexy Darius <114874134+AlexyDarius@users.noreply.github.com>
2024-07-17 16:39:34 +0200

    Add files via upload

--- login.js
+++ login.js
@@ -1,17 +1,53 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    // Define the correct username and password
-    const correctUsername = 'admin';
-    const correctPassword = 'password123';
-
-    if (username === correctUsername && password === correctPassword) {
-        document.getElementById('loginContainer').style.display = 'none';
-        document.getElementById('protectedContent').style.display = 'block';
+document.addEventListener('DOMContentLoaded', (event) => {
+    if (getCookie('loggedIn') === 'true') {
+        showProtectedContent();
     } else {
-        document.getElementById('errorMessage').textContent = 'Incorrect username or password';
+        showLoginForm();
     }
+
+    document.getElementById('loginForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+
+        // Define the correct username and password
+        const correctUsername = 'H3D';
+        const correctPassword = 'Hydrogenius';
+
+        if (username === correctUsername && password === correctPassword) {
+            setCookie('loggedIn', 'true', 30);
+            showProtectedContent();
+        } else {
+            document.getElementById('errorMessage').textContent = 'Incorrect username or password';
+        }
+    });
 });
+
+function showProtectedContent() {
+    document.getElementById('loginContainer').style.display = 'none';
+    document.getElementById('protectedContent').style.display = 'block';
+}
+
+function showLoginForm() {
+    document.getElementById('loginContainer').style.display = 'block';
+    document.getElementById('protectedContent').style.display = 'none';
+}
+
+function setCookie(name, value, minutes) {
+    const date = new Date();
+    date.setTime(date.getTime() + (minutes * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+function getCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
